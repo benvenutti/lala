7a9022abcf39ef1dc73ba518ee65600f5d749b8b
Diogo Benvenutti <diogo.a.benvenutti@gmail.com>
2018-11-11 13:40:30 +0100

    Run pip3

--- modules/Findlibpd.cmake
+++ modules/Findlibpd.cmake
@@ -2,13 +2,13 @@ add_library( libpd SHARED IMPORTED GLOBAL )
 
 set( libPath ${CMAKE_SOURCE_DIR}/3rd-party/libpd )
 
-set_target_properties( libpd 
-    PROPERTIES 
-    IMPORTED_LOCATION ${libPath}/bin/libpd.so )
-
-target_include_directories( libpd
-    INTERFACE 
+set( libpdIncludes
     ${libPath}/cpp
     ${libPath}/libpd_wrapper 
     ${libPath}/libpd_wrapper/util 
     ${libPath}/pure-data/src )
+
+set_target_properties( libpd
+    PROPERTIES
+    IMPORTED_LOCATION ${libPath}/bin/libpd.so
+    INTERFACE_INCLUDE_DIRECTORIES "${libpdIncludes}" )
